64a1234361a6e05c23b4adb6731c43971315a4ee
robbcolo <ybor69@ymail.com>
2025-03-20 10:22:01 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/colombo-dental-web/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')

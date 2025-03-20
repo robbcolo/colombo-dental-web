17fb25f5ae6d0580e0be40fdf17b5b37d9a93af2
robbcolo <ybor69@ymail.com>
2025-03-20 10:48:06 +0100

    ok

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
       <div
         className="absolute inset-0 bg-cover bg-center z-0"
         style={{
-          backgroundImage: `url("/images/about/studio2.jpg")`,
+          backgroundImage: `url(${import.meta.env.BASE_URL}images/about/studio2.jpg)`,
           backgroundSize: 'cover',
           filter: 'brightness(0.50)'
         }}

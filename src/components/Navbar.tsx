2c0ab9431c5f47d1141301e868bbda8f167f0cd8
robbcolo <ybor69@ymail.com>
2025-03-22 14:50:51 +0100

    Logo navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,7 +33,11 @@ const Navbar = () => {
     >
       <div className="container mx-auto px-6 flex justify-between items-center">
         <Link to="/" className="relative flex items-center z-10">
-        <img src="/images/about/logo.png" alt="Logo Studio Dentistico Colombo" className="h-10 w-auto mr-2" />
+          <img
+            src={`${import.meta.env.BASE_URL}images/about/logo.png`}
+            alt="Logo Studio Dentistico Colombo"
+            className="h-10 w-auto mr-2"
+          />
           <span className="text-2xl font-bold text-dental">Studio Dentistico</span>
           <span className="text-2xl font-bold ml-2 text-blue-500">Colombo</span>
         </Link>

f05cbd74588791ba32e3eb18795973574c16fa6c
robbcolo <ybor69@ymail.com>
2025-06-13 11:03:56 +0200

    servizi sistemati

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -4,34 +4,40 @@ import { ArrowRight } from 'lucide-react';
 
 const serviceItems = [
   {
-    icon: <img src={`${import.meta.env.BASE_URL}images/services/dentista.png`} className="w-10 h-10 md:w-12 md:h-12" alt="Odontoiatria Generale" />,
+    icon: <img src={`${import.meta.env.BASE_URL}images/services/abl.png`} className="w-full h-full md:w-full md:h-full" alt="Ablazione" />,
     title: "Igiene Dentale",
     description: "Check-up completi, pulizia dei denti, trattamenti canalari e otturazioni per mantenere la tua bocca sana.",
     link: "/trattamenti/igiene-dentale"
   },
   {
-    icon: <img src={`${import.meta.env.BASE_URL}images/services/impla.png`} className="w-10 h-10 md:w-12 md:h-12" alt="Implantologia" />,
+    icon: <img src={`${import.meta.env.BASE_URL}images/services/impl.png`} className="w-full h-full md:w-full md:h-full" alt="Implantologia" />,
     title: "Implantologia",
     description: "Soluzioni permanenti per denti mancanti con impianti di alta qualità e tecniche minimamente invasive.",
     link: "/trattamenti/implantologia"
   },
   {
-    icon: <img src={`${import.meta.env.BASE_URL}images/services/orto1.png`} className="w-10 h-10 md:w-12 md:h-12" alt="Ortodonzia" />,
+    icon: <img src={`${import.meta.env.BASE_URL}images/services/orto.png`} className="w-full h-full md:w-full md:h-full" alt="Ortodonzia" />,
     title: "Ortodonzia",
     description: "Apparecchi tradizionali e invisibili per correggere l'allineamento dei denti e migliorare il tuo sorriso.",
     link: "/trattamenti/ortodonzia"
   },
   {
-    icon: <img src={`${import.meta.env.BASE_URL}images/services/sbiancamento.png`} className="w-10 h-10 md:w-12 md:h-12" alt="Estetica Dentale" />,
+    icon: <img src={`${import.meta.env.BASE_URL}images/services/estetica.png`} className="w-full h-full md:w-full md:h-full" alt="Estetica" />,
     title: "Estetica Dentale",
     description: "Sbiancamento, faccette e trattamenti estetici per un sorriso perfetto e luminoso.",
     link: "/trattamenti/estetica-dentale"
   },
   {
-    icon: <img src={`${import.meta.env.BASE_URL}images/services/protesi.png`} className="w-10 h-10 md:w-12 md:h-12" alt="Protesi dentale" />,
+    icon: <img src={`${import.meta.env.BASE_URL}images/services/pro.png`} className="w-full h-full md:w-full md:h-full" alt="Protesi" />,
     title: "Protesi dentale",
-    description: "Cure speciali per i più piccoli in un ambiente accogliente che li mette a loro agio.",
-    link: "/trattamenti/odontoiatria-pediatrica"
+    description: "Soluzioni su misura per ripristinare denti mancanti.",
+    link: "/trattamenti/protesi-dentali"
+  },
+    {
+    icon: <img src={`${import.meta.env.BASE_URL}images/services/endo.png`} className="w-full h-full md:w-full md:h-full" alt="Endodonzia" />,
+    title: "Endodonzia",
+    description: "Trattamento dei canali radicolari per salvare denti compromessi.",
+    link: "/trattamenti/endodonzia"
   }
 ];
 
